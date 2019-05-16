81820369b163426a100f4090706d67d09d6fad3e
xavi-b <developer@xavi-b.fr>
2019-05-16 19:26:13 +0200

    dunst

--- deploy.py
+++ deploy.py
@@ -7,5 +7,5 @@ with open(".gitignore","r") as file:
     for line in file:
         line = line.strip();
         if line.startswith("!") and not ".gitignore" in line and not os.path.basename(__file__) in line:
-            print("Copying " + line[1:] + " : " + str(subprocess.call("cp -rT " + line[1:] + " ~/.config/openbox/" + line[1:], shell=True)));
+            print("Copying " + line[1:] + " : " + str(subprocess.call("cp -rT " + line[1:] + " ~/.config/" + line[1:], shell=True)));
 print("Done");
